e746a472798fd3370cb38ac632e29e1e29b1ba6d
Viktoriya Masyuk <viktoriyamasyuk@yandex.ru>
2023-01-27 23:27:29 +0500

    change rewiev's mistakes

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,32 @@
+let popup = document.querySelector('.popup');
+let nameInput = document.querySelector('.form__personal-info_name');
+let jobInput = document.querySelector('.form__personal-info_job');
+let form = document.querySelector('.form');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let buttonEditProfile = document.querySelector('.profile__edit-button');
+let buttonCloseForm = document.querySelector('.form__close');
+
+//Функция открывает форму и заполняет данные из полей секции 'profile'
 function showPopupForm () {
-    let popup = document.querySelector('.popup');
-    popup.classList.remove('popup_opened'); 
+    popup.classList.add('popup_opened');
+    nameInput.textContent = profileName.value;
+    jobInput.textContent = profileJob.value;
 } 
 
-let editProfileButton = document.querySelector('.profile__edit-button');
-editProfileButton.addEventListener('click', showPopupForm)
-
+//Функция закрывает форму
 function hidePopupForm () {
-    let popup = document.querySelector('.popup');
-    popup.classList.add('popup_opened'); 
+    popup.classList.remove('popup_opened'); 
 }
 
-let closeFormButton = document.querySelector('.form__close');
-closeFormButton.addEventListener('click', hidePopupForm); 
-
-
-let form = document.querySelector('.form');
-let nameInput = document.querySelector('.form__name');
-let jobInput = document.querySelector('.form__profession');
-
-console.log(form);
-console.log(nameInput);
-console.log(jobInput);
-
+//Функция переопределяет стандартное поведение формы, вносит изменения в поля секции 'profile' и закрывает форму
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    let newName = document.querySelector('.profile__title');
-    let newJob = document.querySelector('.profile__subtitle');
-    newName.textContent = nameInput.value;
-    newJob.textContent = jobInput.value;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     hidePopupForm();    
 }
 
+buttonEditProfile.addEventListener('click', showPopupForm);
+buttonCloseForm.addEventListener('click', hidePopupForm); 
 form.addEventListener('submit', handleFormSubmit);
